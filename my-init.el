6dd03e593ac9dbd3efd7b77899e7acee64d81452
JavaCommons Technologies <javacommons@gmail.com>
2026-02-02 07:11:38 +0900

    .

--- my-init.el
+++ my-init.el
@@ -191,9 +191,12 @@ app. The app is chosen from your OS's preference."
   (set-buffer-file-coding-system 'utf-8-unix)
   (delete-trailing-whitespace)
   ;;(view-mode-enter)
-  (unless viper-mode
-    (call-interactively #'viper-change-state-to-vi)
-    )
+  ;; (unless (key-binding viper-mode
+  ;;   (call-interactively #'viper-change-state-to-vi)
+  ;;   )
+  (unles (eq (key-binding (kbd "SPC")) 'self-insert-command)
+         (call-interactively #'viper-toggle-key-action)
+         )
   )
 
 ;; https://stackoverflow.com/questions/5154309/how-to-make-a-opened-buffer-read-only-without-reloading-again-with-find-file-re
