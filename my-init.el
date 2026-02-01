fab77f54ef00e326c62e3107704a9c80f2651387
JavaCommons Technologies <javacommons@gmail.com>
2026-02-01 20:49:10 +0900

    .

--- my-init.el
+++ my-init.el
@@ -1,4 +1,24 @@
+;; [straight.el]
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package '(c-quick :type git :host github :repo "emacs-pkg/c-quick"))
 (require 'c-quick)
+
+(straight-use-package '(xprint :type git :host github :repo "emacs-pkg/xprint"))
 (require 'xprint)
 
 (xdump (c-quick-list-non-special-buffers))
